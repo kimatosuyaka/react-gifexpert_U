3e0d0996bc49c7b608f8518c9df6514859d0ee71
Kimatosuyaka <kimatosuyaka@gmail.com>
2021-06-26 12:55:09 -0400

    cambio con WELCOME

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export const AddCategory = ({setCategories}) => {
   
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("Welcome");
   
   const handleInputChange = (e) => {
     setInputValue(e.target.value)
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -5,7 +5,7 @@ export const getGif =async (category) => {
     const api_key = 'KRd2DFMmJAq2pfDvOGO55aWUYQSbPQNL';
     const url = `http://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${ encodeURI(category) }&limit=4`;
 
-    const resp = await fetch( url );//indico que se puede verificar con resp.ok?
+    const resp = await fetch( url ); //indico que se puede verificar con resp.ok?
     const {data} = await resp.json(); 
 
     const gifs = data.map( img => {
